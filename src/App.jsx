fc93ee4f199e37a53ef170a2785069c80e7f2b1e
Nikunj <nikunjraghav24@gmail.com>
2023-12-12 23:39:52 +0000

    Navbar componet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Tailwind CSS</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
