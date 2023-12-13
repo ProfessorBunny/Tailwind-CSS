41c0224973628afb51060abfa413679e0adbc811
Nikunj <nikunjraghav24@gmail.com>
2023-12-13 00:01:22 +0000

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-emerald-100">
+      <div className="mx-auto">hey</div>
+    </nav>
+  );
 };
 
 export default Navbar;
